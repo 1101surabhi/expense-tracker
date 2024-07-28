1440347fc76324cbcbaccdcd2d034fd9646173d6
1101surabhi <956surabhi.ag@gmail.com>
2024-07-28 22:09:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import Expenses from "./components/Expenses";
+import './components/Expenses.css'
 
 function App() {
+  const expenses = [
+    {
+      title: "Smartphone",
+      amount: 20000,
+      date: new Date("1-7-2022"),
+    },
+    {
+      title: "Books",
+      amount: 1000,
+      date: new Date("3-3-2019"),
+    },
+    {
+      title: "Groceries",
+      amount: 350,
+      date: new Date("6-25-2024"),
+    },
+    {
+      title: "Internet",
+      amount: 2000,
+      date: new Date("2-16-2024"),
+    },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Hello</h1>
+      <div className="expenses">
+        <Expenses expenses={expenses} />
+      </div>
     </div>
   );
 }
